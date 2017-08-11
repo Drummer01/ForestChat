72d2a8c882db0dcbe46cadf85f0103cd36a1bfbe
Drummer01 <self1995@mail.ua>
2017-08-11 21:45:59 +0300

    [*] Channel owning auth

---
+++ app/Containers/Channel/Traits/ChannelOwning.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Drummer
+ * Date: 11.08.2017
+ * Time: 16:23
+ */
+
+namespace App\Containers\Channel\Traits;
+
+
+use App\Containers\Channel\Models\Channel;
+use App\Containers\Channel\Models\ChannelRole;
+use App\Containers\User\Models\User;
+
+trait ChannelOwning
+{
+    /**
+     * @param User $user
+     * @param Channel $channel
+     * @return bool
+     */
+    protected function isOwnChannelOrHasAdminRole(User $user, Channel $channel)
+    {
+        return $user->id === $channel->creator_id
+            || $user->hasRole('Admin')
+            || $user->hasChannelRole(ChannelRole::ADMINISTRATOR, $channel);
+    }
+}
