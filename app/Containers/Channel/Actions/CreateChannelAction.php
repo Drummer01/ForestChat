1b2210c74679385d27ef380eda19d8f7e37a57c3
Drummer01 <self1995@mail.ua>
2017-08-14 21:22:20 +0300

    [+] Assign administrator role to channel owner

--- app/Containers/Channel/Actions/CreateChannelAction.php
+++ app/Containers/Channel/Actions/CreateChannelAction.php
@@ -5,6 +5,8 @@ namespace App\Containers\Channel\Actions;
 use App\Containers\Authentication\Tasks\GetAuthenticatedUserTask;
 use App\Containers\Channel\Tasks\CreateChannelTask;
 use App\Containers\Channel\UI\API\Requests\CreateChannelRequest;
+use App\Containers\ChannelAuthorization\Tasks\AssignUserToChannelRoleTask;
+use App\Containers\ChannelAuthorization\Tasks\GetChannelRoleTask;
 use App\Ship\Parents\Actions\Action;
 
 class CreateChannelAction extends Action
@@ -16,11 +18,18 @@ class CreateChannelAction extends Action
     public function run(CreateChannelRequest $request)
     {
         $user = $this->call(GetAuthenticatedUserTask::class, []);
-        return $this->call(CreateChannelTask::class, [
+
+        $channel = $this->call(CreateChannelTask::class, [
             $user->id,
             $request->name,
             $request->password,
             $request->image_url
         ]);
+
+        //assign administrator role to channel owner
+        $adminRole = $this->call(GetChannelRoleTask::class, [null, 'administrator']);
+        $this->call(AssignUserToChannelRoleTask::class, [$user, $channel, $adminRole]);
+
+        return $channel;
     }
 }
