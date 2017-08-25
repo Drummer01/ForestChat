6a7356a7c07233989eecac744365f3ff781a45d6
Drummer01 <xpaand4@gmail.com>
2017-08-25 18:41:04 +0300

    Add default channel roles on channel creation

--- app/Containers/Channel/Actions/CreateChannelAction.php
+++ app/Containers/Channel/Actions/CreateChannelAction.php
@@ -6,11 +6,26 @@ use App\Containers\Authentication\Tasks\GetAuthenticatedUserTask;
 use App\Containers\Channel\Tasks\CreateChannelTask;
 use App\Containers\Channel\UI\API\Requests\CreateChannelRequest;
 use App\Containers\ChannelAuthorization\Tasks\AssignUserToChannelRoleTask;
+use App\Containers\ChannelAuthorization\Tasks\CreateChannelRoleTask;
 use App\Containers\ChannelAuthorization\Tasks\GetChannelRoleTask;
 use App\Ship\Parents\Actions\Action;
 
 class CreateChannelAction extends Action
 {
+
+    private $defaultChannelRolesData = [
+        [
+            'name' => 'administrator',
+            'display_name' => 'Administrator',
+            'color' => '#073b8e'
+        ],
+        [
+            'name' => 'moderator',
+            'display_name' => 'Moderator',
+            'color' => '#870484'
+        ]
+    ];
+
     /**
      * @param CreateChannelRequest $request
      * @return mixed
@@ -26,8 +41,17 @@ class CreateChannelAction extends Action
             $request->image_url
         ]);
 
+        /**
+         * Create default channel roles for newly created channel
+         * Administrator, Moderator
+         */
+        foreach ($this->defaultChannelRolesData as $roleData) {
+            $roleData['channel_id'] = $channel->id;
+            $this->call(CreateChannelRoleTask::class, [$roleData]);
+        }
+
         //assign administrator role to channel owner
-        $adminRole = $this->call(GetChannelRoleTask::class, [null, 'administrator']);
+        $adminRole = $this->call(GetChannelRoleTask::class, [$channel, 'administrator']);
         $this->call(AssignUserToChannelRoleTask::class, [$user, $channel, $adminRole]);
 
         return $channel;
